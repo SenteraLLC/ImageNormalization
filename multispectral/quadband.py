f549a9e00d86a1dfd1412026a0a7a8b1b5759ae2
Andrew Muehlfeld <andrew.muehlfeld@sentera.com>
2017-08-31 16:54:10 -0500

    Add further image corrections
    - Use numpy for array operations
    - Add shutter efficiency correction
    - Add leakage light correction
    - Add quantum efficiency correction (coefficients from specific data set)
    
    [Not in JIRA]

--- multispectral/quadband.py
+++ multispectral/quadband.py
@@ -9,6 +9,7 @@
 import csv
 import os
 import sys
+import numpy as np
 
 from PIL import Image, ImageMath
 
@@ -16,6 +17,17 @@ PIX4DCSV_FILENAME = 'pix4d.csv'
 NEWPIX4DCSV_FILENAME = 'newpix4d.csv'
 AUTOEXPOSURECSV_FILENAME = 'autoexposure.csv'
 
+# 655nm
+# LEAKAGE_LIGHT_FACTOR = 0.05
+# QE_FACTOR = 18.53
+
+# 725nm
+# LEAKAGE_LIGHT_FACTOR = 0.085
+# QE_FACTOR = 46.57
+
+# 800nm
+LEAKAGE_LIGHT_FACTOR = 0.12
+QE_FACTOR = 60.29
 
 def post_process(dirs, processed_subdir='processed'):
 	for dirstring in dirs:
@@ -60,6 +72,8 @@ def post_process(dirs, processed_subdir='processed'):
 
 		# TODO: The value of this constant.
 		k = 1.
+		frame_period_us = 22000
+		shutter_efficiency = 1/650
 
 		# TODO: Graphical Progress Update
 		for jpgfile, csvrow in zip(jpgfiles, csvreader):
@@ -78,7 +92,27 @@ def post_process(dirs, processed_subdir='processed'):
 			im.close()
 
 			temp = ImageMath.eval("float(a)", a=img)
-			out = temp.point(lambda i: i*cal)
+			nimg = np.asarray(temp, dtype=np.float32)
+
+			# Apply Shutter Efficiency Correction
+			shutter_efficiency_correction_factor = 1 / (1 + ((shutter_efficiency * frame_period_us) / int(csvrow[1])))
+			nimg = nimg * shutter_efficiency_correction_factor
+
+			# Apply Leakage Light Correction
+			total = 0
+			mean = nimg.mean()
+			offset = LEAKAGE_LIGHT_FACTOR * mean
+			nimg = nimg - offset
+
+
+			# Apply Exposure Opportunity Correction
+			nimg = nimg * cal
+
+			# Apply Reflectance Scaling
+			nimg = nimg * QE_FACTOR
+
+			# Convert numpy to pillow
+			out = Image.fromarray(nimg)
 			
 			outfile = os.path.join(outdirstring, jpgfile[0:len(jpgfile)-4] + '.tif')
 			out.save(outfile, "TIFF")
